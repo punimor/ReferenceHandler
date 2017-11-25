973c91901c08690eafbe23b2dc6c9aed900e963c
Josiah Coad <josiah@coad.net>
2017-11-25 22:33:33 +0000

    changes

--- WebsitePages/javascript/import.js
+++ WebsitePages/javascript/import.js
@@ -3,37 +3,6 @@ paper = {"rating": 3.0,
          "abstract" : "BACKGROUND: ", 
          "decision" : "Include"}
 
-
-function import_handler() {
-    alert("Hello World");
-}
-
-// Popup which allows user to select a material
-function materialSelect() {
-    swal({
-      title: "Our Materials",
-      text: '<div id="placeholder"></div>',
-      showCancelButton: true,
-      showConfirmButton: false,
-      html: true
-    });
-    $("#placeholder").load("popups/materialselect.html");
-}
-
-// function OnFileLoad() {
-//     var file = document.getElementById("FileReader").files[0];
-//     var textType = /text.*/;
-//     if (file.type.match(textType)) {
-//         var reader = new FileReader();
-//         reader.onload = function (e) {
-//             documentFile = reader.result;
-//             alert("her")
-//         }
-//     } else {
-//         alert("File not supported!")
-//     }
-// }
-
 window.onload = function() {
     var fileInput = document.getElementById('fileInput');
 
@@ -62,7 +31,8 @@ function sendToServer(data){
             success=function( response ) {
               // console.log(typeof(response));
               parsed_document = JSON.parse(response)
-              console.log(parsed_document.length + " papers parsed.")
+              // console.log(parsed_document.length + " papers parsed.")
+              console.log(window.glob)
             });
 }
 
