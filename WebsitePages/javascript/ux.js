50d5727d4915ba52e5800b44a430cae115662a9f
Josiah Coad <josiah@coad.net>
2017-11-25 22:21:48 +0000

    yayy

--- WebsitePages/javascript/ux.js
+++ WebsitePages/javascript/ux.js
@@ -13,13 +13,3 @@ $("#menu-toggle").click(function(e) {
 $('.collapse').on('show.bs.collapse', function () {
 $('.collapse.in').collapse('hide');
 });
-
-// Slider
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
