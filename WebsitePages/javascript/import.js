9e56eb24b71068324b29d7db0b33f2aebbfc122f
punimor <filip.gnesin@gmail.com>
2017-11-26 04:17:17 +0000

    j

--- WebsitePages/javascript/import.js
+++ WebsitePages/javascript/import.js
@@ -24,8 +24,13 @@ window.onload = function() {
       }
     });
 }
+paper = {"rating": 3.0, 
+         "title" : "Cardiovascular safety of new drugs for diabetes: Getting the balance right?", 
+         "abstract" : "BACKGROUND: Skin autofluorescence is a non-invasive measurement of advanced glycation end products (AGE), which are suggested to be one of the major agents in the pathogenesis and progression of diabetes related cardiovascular complications. Recently, low vitamin D status has been linked to the progression of type 2 diabetes mellitus (T2DM) and cardiovascular disease. The aim of this study is to investigate the association between vitamin D status and skin autofluorescence in patients with T2DM. METHODS: In this preliminary report skin autofluorescence was measured non-invasively with an AGE-reader in 245 patients with T2DM treated with lifestyle advice, metformin and/or sulphonylurea-derivatives. All patients were randomly assigned to receive either vitamin D 50,000 IU/month or placebo for 6 months. RESULTS: Skin autofluorescence was significantly higher in patients with a serum 25(OH)D < 50 nmol/l compared to patients with a serum 25(OH)D > 75 nmol/l (2.81 versus 2.41; p < 0.001). Mean serum 25(OH)D was 60.3 +/- 23.4 nmol/l and was independently associated with skin autofluorescence (beta -0.006; p < 0.001). Mean vitamin D increased from 60.8 to 103.6 nmol/l in the intervention group, however no effect was seen on accumulation of skin AGEs after 6 months compared to placebo. CONCLUSIONS: Vitamin D status is independently associated with skin auto fluorescence in patients with well-controlled T2DM. No effect was seen on the amount of skin AGEs after a short period of 6 months vitamin D supplementation. Further research with longer follow-up and measurement of circulating advanced glycation end products is needed to elucidate the causality of the association.",
+                    "decision" : "Include"}
 
-function sendToServer(data){
+allpapers = [paper,paper,paper,paper]
+function sendToServer(data){/*
     $.post( "http://localhost:5000/upload", 
             input = JSON.stringify(data), 
             success=function( response ) {
@@ -34,18 +39,49 @@ function sendToServer(data){
               $("#global").text(response)
               var allpapers = display_papers(parsed_document)
             }); 
+*/
+
+    display_papers(allpapers)
 }
 
 
 function display_papers(allpapers){
     var i;
     for (i in allpapers) {
-        var markup = "<tr class='unselected' data-toggle='collapse' data-target='.collapsingItem" + String(i) + "'><td id ='table-rating'>" + Math.round(parseFloat(allpapers[i]['rating']) * 10) / 10 + "</td><td><h5>" + allpapers[i]['title'] + "</h5><div class='collapse collapsingItem" + String(i) + "'>" + allpapers[i]['abstract'] + "</div></td><td><button type='button' class='btn btn-success'>Include</button><div class='collapse collapsingItem" + String(i) + "'><button type='button' class='btn btn-outline-success'>Include</button><button type='button' class='btn btn-outline-info'>Maybe</button><button type='button' class='btn btn-outline-danger'>Exclude</button></div></td></tr>"
+        var markup = "<tr class='unselected' data-toggle='collapse' data-target='.collapsingItem" + String(i) + "'><td id ='table-rating'>" + "<div class='rateYo" + String(i) + "'></div>" + "</td><td><h5>" + allpapers[i]['title'] + "</h5><div class='collapse collapsingItem" + String(i) + "'>" + allpapers[i]['abstract'] + "</div></td><td><button type='button' class='btn btn-outline-secondary btn-block undecide decisionbutt'></button><div class='collapse collapsingItem" + String(i) + "'><button type='button' class='btn btn-outline-success btn-block button-adjust include'></button><button type='button' class='btn btn-outline-info btn-block button-adjust maybe'></button><button type='button' class='btn btn-outline-danger btn-block button-adjust exclude'></button></div></td></tr>"
         $("table tbody").append(markup);
     }
+
+    // rating papers
+    $(function () {
+
+    $(".rateYo0").rateYo({
+    rating: Math.round(parseFloat(allpapers[i]['rating']) * 10) / 10
+    });
+
+    // Setter
+    $(".rateYo0").rateYo("option", "starWidth", "20px"); //returns a jQuery Element
+    });
+
+
     $("tr").click(function(e){
         $("tbody").children().removeClass('selected');
         $(this).addClass('selected');
     });
 
+    // button functionality
+    $(".include").click(function(e){
+        $(this).parent().parent().find(".decisionbutt").removeClass().addClass("btn btn-success btn-block include decisionbutt");
+        // $("#decisionbutton1").removeClass().addClass("btn btn-success include");
+    });
+
+    $(".maybe").click(function(e){
+        $(this).parent().parent().find(".decisionbutt").removeClass().addClass("btn btn-info btn-block maybe decisionbutt");
+        // $("#decisionbutton1").removeClass().addClass("btn btn-info maybe");
+    });
+
+    $(".exclude").click(function(e){
+        $(this).parent().parent().find(".decisionbutt").removeClass().addClass("btn btn-danger btn-block exclude decisionbutt");
+        // $("#decisionbutton1").removeClass().addClass("btn btn-danger exclude");
+    });
 }
