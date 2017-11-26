d5d7bc465d4939ba5d3627ac4c1431c20c3b9109
Josiah Coad <josiah@coad.net>
2017-11-26 00:07:05 +0000

    adf

--- WebsitePages/javascript/import.js
+++ WebsitePages/javascript/import.js
@@ -31,7 +31,21 @@ function sendToServer(data){
             success=function( response ) {
               parsed_document = JSON.parse(response)
               // console.log(parsed_document.length + " papers parsed.")
-              console.log(window.glob)
-            });
+              $("#global").text(response)
+              var allpapers = display_papers(parsed_document)
+            }); 
 }
 
+
+function display_papers(allpapers){
+    var i;
+    for (i in allpapers) {
+        var markup = "<tr class='unselected' data-toggle='collapse' data-target='#abstract" + String(i) + "'><td id ='table-rating'>" + Math.round(parseFloat(allpapers[i]['rating']) * 10) / 10 + "</td><td><h5>" + allpapers[i]['title'] + "</h5><div id='abstract" + String(i) + "' class='collapse'>" + allpapers[i]['abstract'] + "</div></td><td>" + allpapers[i]['decision'] + "</td></tr>"
+        $("table tbody").append(markup);
+    }
+    $("tr").click(function(e){
+        $("tbody").children().removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+}
