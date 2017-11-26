406949c92b3b39996218a1c6a579c982d5988e00
Josiah Coad <josiah@coad.net>
2017-11-26 04:16:14 +0000

    adf

--- Backend/PaperScorer.py
+++ Backend/PaperScorer.py
@@ -15,21 +15,23 @@ class PaperScorer:
                 self.desired_phrase_pairs = [DesiredPhrase(float(line.split(",")[0]), [word.strip() for word in line.split(",")[1:]]) for line in f.readlines()]
 
     def get_scored_papers(self):
+        augmented_papers = []
         for paper in self.papers:
-            paper.sections["score_matches"] = []
-            paper.sections["rating"] = self.calculate_score(paper)
-        return [paper.sections for paper in sorted(self.papers, key=lambda p: p.sections["rating"], reverse=True)]
+            augmented_papers.append(self.calculate_score(paper))
+        return [paper.sections for paper in sorted(augmented_papers, key=lambda p: p.sections["rating"], reverse=True)]
 
     def calculate_score(self, paper):
         # Score the document based on the desired words
         score_sum = 0
+        paper.sections["score_matches"] = []
         for desired_phrase_obj in self.desired_phrase_pairs:
             for desired_phrase in desired_phrase_obj.words:
                 if desired_phrase in paper.sections["abstract"] or desired_phrase in paper.sections["title"]:
                     score_sum += desired_phrase_obj.weight
                     paper.sections["score_matches"].append((desired_phrase, desired_phrase_obj.weight))
                     divclass = "bad_word" if desired_phrase_obj.weight <= 0 else "good_word"
-                    paper.sections["abstract"].replace(desired_phrase, "<div class="+divclass+">"+desired_phrase+"</div>")
-                    paper.sections["title"].replace(desired_phrase, "<div class="+divclass+">"+desired_phrase+"</div>")
+                    paper.sections["abstract"] = paper.sections["abstract"].replace(desired_phrase, "<span class="+divclass+">"+desired_phrase+"</span>")
+                    paper.sections["title"] = paper.sections["title"].replace(desired_phrase, "<span class="+divclass+">"+desired_phrase+"</span>")
                 break
-        return score_sum
+        paper.sections["rating"] = score_sum
+        return paper
